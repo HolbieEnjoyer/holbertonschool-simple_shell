56160b16624a72c616ff22acb3b1888bfd0a3cd5
Your Name <you@example.com>
2024-05-08 20:58:29 +0000

    Corecting mistakes

--- execute.c
+++ execute.c
@@ -39,38 +39,46 @@ int execute(char *command)
 	char *arr[64], **env = environ, *path = getenv("PATH"), *token, exepath[256];
 	pid_t pid = fork();
 
-	if (pid == -1)
-		perror("fork"), free(command), exit(EXIT_FAILURE);
-	else if (pid == 0)
-	{
-		line_div(command, arr);
-		arr[0] == NULL ? free(command), exit(EXIT_SUCCESS) : placeholder();
-		if (strcmp(arr[0], "env") == 0)
-		{
-			while (*env != NULL)
-				printf("%s\n", *env), env++;
-			free(command), exit(EXIT_SUCCESS);
-		}
-		if (strchr(arr[0], '/') != NULL)
-			execve_error(arr, command);
-		else
-		{
-			if (path == NULL)
-				fprintf(stderr, ERR_STR, arr[0]), free(command), exit(127);
-			token = strtok(path, ":");
-			while (token != NULL)
-			{
-				snprintf(exepath, sizeof(exepath), "%s/%s", token, arr[0]);
-				execve_error(arr, command)
-				token = strtok(NULL, ":");
-			}
-		}
-		fprintf(stderr, ERR_STR, arr[0]), free(command), exit(127);
-	}
-	else
-	{
-		waitpid(pid, &status, 0), free(command);
-		status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
-	}
-	return (status);
+	if (pid == -1) {
+    perror("fork");
+    free(command);
+    exit(EXIT_FAILURE);
+} else if (pid == 0) {
+    line_div(command, arr);
+    if (arr[0] == NULL) {
+        free(command);
+        exit(EXIT_SUCCESS);
+    } else {
+        if (strcmp(arr[0], "env") == 0) {
+            while (*env != NULL) {
+                printf("%s\n", *env);
+                env++;
+            }
+            free(command);
+            exit(EXIT_SUCCESS);
+        }
+        if (strchr(arr[0], '/') != NULL) {
+            execve_error(arr, command);
+        } else {
+            if (path == NULL) {
+                fprintf(stderr, ERR_STR, arr[0]);
+                free(command);
+                exit(127);
+            }
+            token = strtok(path, ":");
+            while (token != NULL) {
+                snprintf(exepath, sizeof(exepath), "%s/%s", token, arr[0]);
+                execve_error(arr, command); // Added missing semicolon here
+                token = strtok(NULL, ":");
+            }
+        }
+        fprintf(stderr, ERR_STR, arr[0]);
+        free(command);
+        exit(127);
+    }
+} else {
+    waitpid(pid, &status, 0);
+    free(command);
+    status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
 }
+
